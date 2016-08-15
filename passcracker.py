066371995f0f4c942c1af39856a3ccb017d291ba
Vibhor <bijoy.vibhor19@gmail.com>
2016-08-15 17:30:40 +0530

    Add files via upload

---
+++ passcracker.py
@@ -0,0 +1,22 @@
+import md5
+counter = 1
+
+pass_in = raw_input("Please enter the MD5 Hash:")
+pwfile  = raw_input("Please enter the password file name:")
+
+try:
+    pwfile = open(pwfile,"r")
+except:
+     print("\nFile Not Found.")
+     exit()
+
+for password in pwfile:
+    filemd5 = md5.new(password.strip()).hexdigest()
+    print("Trying password number %d: %s"%(counter, password.strip())
+
+    counter +=1
+
+    if pass_in == filemd5:
+           print("\nMatch Found. \nPassword is: %s" %password)
+           break
+else: print("\nPassword Not Found.")
